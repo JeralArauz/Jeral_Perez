6ca30c2a4dc683966a4f955ac499d6ff43dfb1e0
Jeral Aráuz <gerald08.kk@gmail.com>
2022-02-26 16:56:19 -0600

    Se agregaron validaciones.

---
+++ Jeral_Perez/wwwroot/js/EliminarCliente.js
@@ -0,0 +1,19 @@
+﻿$("#eliminarcliente").click(function () {
+    Swal.fire({
+        title: '¿Esta seguro?',
+        text: "Esta acción es irreversible!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, Eliminar!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                'Borrado!',
+                'Los datos han sido borrados exitosamente.',
+                'success'
+            )
+        }
+    })
+})
